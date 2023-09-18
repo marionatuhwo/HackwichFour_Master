3f8f31494482848b42975e3b0be64b43291a89e0
marionatuhwo <ano@hawaii.edu>
2023-09-18 12:10:23 -1000

    Initial Commit

---
+++ HackwichFour_Master/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HackwichFour_Master
+//
+//  Created by Marion Ano on 9/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HackwichFour_MasterUITests/HackwichFour_MasterUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HackwichFour_MasterUITestsLaunchTests.swift
+//  HackwichFour_MasterUITests
+//
+//  Created by Marion Ano on 9/18/23.
+//
+
+import XCTest
+
+class HackwichFour_MasterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
